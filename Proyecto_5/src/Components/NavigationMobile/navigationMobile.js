70945473282815b5fd50afffd3b84b9fc7cba225
xforzan <nakhila2024@gmail.com>
2024-12-03 17:25:33 +0100

    pfp

--- Proyecto_5/src/Components/NavigationMobile/navigationMobile.js
+++ Proyecto_5/src/Components/NavigationMobile/navigationMobile.js
@@ -9,7 +9,7 @@ export const navigationMobile = () => {
             <ul>
                 <li class="home"><a href="#home"></a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
+                <li><img src="../../public/account.svg" alt="avatar"</li>
                 <li id="cart" class="cartIcon-container">
                     <img src="./shopping-bag.svg" alt"shopping bag" class="shoppingBag">
                     <span id="cart-count2"></span>
