47662e9817ca98b6eae9769af8b97da64be37849
Achraf Nakhil <nakhila2024@gmail.com>
2024-12-16 21:20:37 +0100

    Update searchListener.js

--- Proyecto_5/src/Utils/searchListener.js
+++ Proyecto_5/src/Utils/searchListener.js
@@ -6,51 +6,27 @@ import { notFound } from "../Pages/NotFound/notFound";
 export const searchListener = () => {
     const searchBar = document.querySelector("#searchBar");
 
-
-    searchBar.addEventListener("input", () => {
-    });
-
-
     searchBar.addEventListener("keydown", (event) => {
-        if (event.key === "Enter") {
+
+        if (event.keyCode === 13) {
+            event.preventDefault();
             const searchValue = searchBar.value.toLowerCase();
-        const filteredProducts = productsList.filter(product => 
-            product.name.toLowerCase().includes(searchValue)
-        );
 
 
-        if (filteredProducts.length === 0) {
-            notFound(searchBar.value);
-        }
-        else {
-        hiddenProducts.length = 0;
-        hiddenProducts.push(...productsList);
-        productsList.length = 0;
-        for (const product of hiddenProducts) {
-            if (filteredProducts.includes(product)) {
-                productsList.push(product);
+            const filteredProducts = productsList.filter(product =>
+                product.name.toLowerCase().includes(searchValue)
+            );
+
+
+            if (filteredProducts.length === 0) {
+                notFound(searchBar.value);
             } else {
 
+                productsList.length = 0;
+                productsList.push(...filteredProducts); 
+
+                products();
             }
-        }
-        products();
-        productsList.length = 0;
-        productsList.push(...hiddenProducts);
-        hiddenProducts.length = 0;
-    }
-        
-
-        }
-
-        
-
-        
-    });
-
-    window.addEventListener("keydown", (event) => {
-        if (event.key === "Enter") {
-            alert("prueba");
         }
     });
 };
-
